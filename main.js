56e54c591a3eb1f848676277c52b658bbbb77f8c
Isadora <87739902+doramrx@users.noreply.github.com>
2022-05-08 14:08:18 -0300

    Landing Page Desktop Version - Stage 5

--- main.js
+++ main.js
@@ -5,6 +5,47 @@ onScroll()
 function onScroll() {
  showNavOnScroll()
  showBackToTopButtonOnScreen()
+
+ activateMenuAtCurrentSection(home)
+ activateMenuAtCurrentSection(services)
+ activateMenuAtCurrentSection(about)
+ activateMenuAtCurrentSection(contact)
+}
+
+//section = home ou services ou sobre
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + innerHeight / 2
+
+ /*verificar se a seção passou da linha*/
+
+ //topo da seção
+ const sectionTop = section.offsetTop
+
+ //altura da seção
+ const sectionHeight = section.offsetHeight
+
+ //o topo da seção chegou ou ultrapassou a linha alto
+ const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+ /*verificar se a base está abaixo da linha alto*/
+
+ //a seção termina onde?
+ const sectionEndsAt = sectionTop + sectionHeight
+
+ const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+ 
+ //limites da seção
+ const sectionBoundaries = 
+ sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+ const sectionId = section.getAttribute('id')
+ const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+ menuElement.classList.remove('active')
+ if(sectionBoundaries){
+  menuElement.classList.add('active')
+ }
+
 }
 
 function showNavOnScroll(){
@@ -22,8 +63,6 @@ function showBackToTopButtonOnScreen(){
     backToTopButton.classList.remove('show')
   }
 }
-
-
 
 function openMenu(){
   document.body.classList.add('menu-expanded')
@@ -47,3 +86,5 @@ ScrollReveal({
            #about,
            #about header,
            #about .content`)
+
+
